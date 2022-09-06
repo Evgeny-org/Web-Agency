3eff0bdc76938a4e65bd1ee5a03ff15e0bb79fef
Евгений <evgenij@MPro-Evgeny.local>
2022-09-06 18:08:01 +0300

    add connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,20 @@
 <?php
-	$DBH = new PDO("mysql:host=localhost;dbname=Web-Agency", "root", "");
-	$STH = $DBH->prepare("INSERT INTO `connection` (`id`, `name`, `surname`, `email`, `comment`) VALUES (NULL, '1', '', '', '');");  
-	$STH->execute();
+	$name = $_POST['name']; //данные их формы
+	$surname = $_POST['surname'];
+	$email = $_POST['email'];
+	$comment = $_POST['comment'];
+
+	$DBH = new PDO("mysql:host=localhost; dbname=Web-Agency; charset=utf8", "root", ""); //подключение к БД
+
+	$options = [
+		'name' => $name,
+		'surname' => $surname,
+		'email' => $email,
+		'comment' => $comment
+	]; //массив с подготовленным данными
+
+	$SQL = "INSERT INTO `connection` (`id`, `name`, `surname`, `email`, `comment`) VALUES (NULL, :name, :surname, :email, :comment)";//готовка запроса с плейсхолдерами
+	$query = $DBH->prepare($SQL);//подготавливаем запрос
+
+	$query->execute($options);//отпралвяем запрос
 ?>
